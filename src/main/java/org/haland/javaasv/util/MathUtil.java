1d17e004a5f51d2714b5adc6bdad140f724b98c7
kevinBecker <kjbecker00@gmail.com>
2022-03-29 13:04:18 -0400

    Kalman filter - init
    Begins adding the kalman filter.
    
    This is just a backup to save some matrix code, will use another library in the next push

--- src/main/java/org/haland/javaasv/util/MathUtil.java
+++ src/main/java/org/haland/javaasv/util/MathUtil.java
@@ -48,3 +48,66 @@ public final class MathUtil {
         return Math.max(low, Math.min(value, high));
     }
 }
+
+    /**
+     * Returns the value of two matricies multiplied together (a * b).
+     * Copied from Jama, adapted to work for 2d arrays
+     * @param a matrix 1.
+     * @param b matrix 2.
+     */
+    public static double [][] mmult (double [][]a, double [][]b){
+        if (b[0].length != a.length) {
+            throw new IllegalArgumentException("Matrix inner dimensions must agree.");
+        }
+        int n = a.length;
+        int m = a[0].length;
+        int bn = b.length;
+        int bm = b[0].length;
+        double[] Bcolj = new double[n];
+        double [][] out = new double [m][bn];
+        for (int j = 0; j < bn; j++) {
+            for (int k = 0; k < n; k++) {
+                Bcolj[k] = b[k][j];
+            }
+            for (int i = 0; i < m; i++) {
+                double[] Arowi = a[i];
+                double s = 0;
+                for (int k = 0; k < n; k++) {
+                    s += Arowi[k]*Bcolj[k];
+                }
+                out[i][j] = s;
+            }
+        }
+        return out;
+}
+
+    /**
+     * Returns the inverse of a matrix.
+     * Copied from Jama, adapted to work for 2d arrays
+     * @param a matrix to be inverted
+     */
+    public static double [][] mmult (double [][]a, double [][]b){
+        if (b[0].length != a.length) {
+            throw new IllegalArgumentException("Matrix inner dimensions must agree.");
+        }
+        int n = a.length;
+        int m = a[0].length;
+        int bn = b.length;
+        int bm = b[0].length;
+        double[] Bcolj = new double[n];
+        double [][] out = new double [m][bn];
+        for (int j = 0; j < bn; j++) {
+            for (int k = 0; k < n; k++) {
+                Bcolj[k] = b[k][j];
+            }
+            for (int i = 0; i < m; i++) {
+                double[] Arowi = a[i];
+                double s = 0;
+                for (int k = 0; k < n; k++) {
+                    s += Arowi[k]*Bcolj[k];
+                }
+                out[i][j] = s;
+            }
+        }
+        return out;
+    }
